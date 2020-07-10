6cb9052d38a1c1de70848ba06fc264515b2ab93b
Rafael Fernandes dos Santos <rafaelfernandes.ti@gmail.com>
2020-07-10 12:32:04 +0100

    Implemented Interfaces and classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Text;
+using System.Xml.Xsl;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Event
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
